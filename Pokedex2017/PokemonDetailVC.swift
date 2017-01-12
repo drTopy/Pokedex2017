1987f552f7aa2fd691e495e06e6f139a36955fe4
Thiago Costa <thiagoscosta@gmail.com>
2017-01-12 08:53:25 -0500

    cleanup

--- Pokedex2017/PokemonDetailVC.swift
+++ Pokedex2017/PokemonDetailVC.swift
@@ -12,7 +12,6 @@ class PokemonDetailVC: UIViewController {
 
     var pokemon: Pokemon!
     
-    
     @IBOutlet weak var nameLbl: UILabel!
     @IBOutlet weak var mainImg: UIImageView!
     @IBOutlet weak var descriptionLbl: UILabel!
@@ -28,16 +27,23 @@ class PokemonDetailVC: UIViewController {
     
     
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        nameLbl.text = pokemon.name
         
-        pokemon.dowloadPokemonDetail {
-   
-            //be called after network call is complete
+        nameLbl.text = pokemon.name.capitalized
+        
+        let img = UIImage(named: "\(pokemon.pokedexId)")
+        
+        mainImg.image = img
+        currentEvoImg.image = img
+        pokedexLbl.text = "\(pokemon.pokedexId)"
+        pokemon.downloadPokemonDetail {
+            
+            // Whatever we write will only be called after the network call is compelte!
             self.updateUI()
         }
-    
+        
     }
     
     func updateUI() {
@@ -46,11 +52,27 @@ class PokemonDetailVC: UIViewController {
         defenseLbl.text = pokemon.defense
         heightLbl.text = pokemon.height
         weightLbl.text = pokemon.weight
+        typeLbl.text = pokemon.type
+        descriptionLbl.text = pokemon.description
         
+        if pokemon.nextEvolutionId == "" {
+            
+            evoLbl.text = "No Evolutions"
+            nextEvoImg.isHidden = true
+            
+        } else {
+            
+            nextEvoImg.isHidden = false
+            nextEvoImg.image = UIImage(named: pokemon.nextEvolutionId)
+            let str = "Next Evolution: \(pokemon.nextEvolutionName) - LVL \(pokemon.nextEvolutionLevel)"
+            evoLbl.text = str
+        }
     }
     
-    @IBAction func backBtnPressed(_ sender: Any) {
+    @IBAction func backBtnPressed(_ sender: UIButton) {
+        
         dismiss(animated: true, completion: nil)
     }
-
+    
+    
 }
